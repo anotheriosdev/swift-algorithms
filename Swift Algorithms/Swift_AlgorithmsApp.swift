06371326c721c266d07cc972b0c630aea56eabe0
Bilal M <bilalmughal.mr@gmail.com>
2024-07-11 16:17:33 +0500

    initial commit
    project setup

---
+++ Swift Algorithms/Swift_AlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_AlgorithmsApp.swift
+//  Swift Algorithms
+//
+//  Created by Muhammad Bilal on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_AlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
